ee55a95a22239b0cf257ed7ce224d8972f2338c4
Helinton Fioramonte <helinton.fioramonte@iatec.com>
2018-01-23 22:09:13 -0200

    Criado projeto do curso CSHARP-AVANCADO

---
+++ Udemy/Csharp-Avancado/S01-Serializacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serializacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
