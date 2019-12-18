3f4f707d6151b962d5a9218264375f16c803bd2f
helinton.fioramonte <helinton.fioramonte@iatec.com>
2019-12-18 15:54:12 -0300

    atualizado

--- Udemy/Csharp-Avancado/Csharp-Avancado/Attributes/MyValidator.cs
+++ Udemy/Csharp-Avancado/Csharp-Avancado/Attributes/MyValidator.cs
@@ -19,5 +19,6 @@ namespace Attributes
             }
             //return base.IsValid(value);
         }
+
     }
 }
--- Udemy/Csharp-Avancado/Csharp-Avancado/LINQ/Program.cs
+++ Udemy/Csharp-Avancado/Csharp-Avancado/LINQ/Program.cs
@@ -7,9 +7,31 @@ namespace LINQ
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
             Console.WriteLine("LINQ e LAMBDA");
+
+
+
+
+
+            // Declare a Func variable and assign a lambda expression to the  
+            // variable. The method takes a string and converts it to uppercase.
+            Func<string, string> selector = str => str.ToUpper();
+
+            // Create an array of strings.
+            string[] words = { "orange", "apple", "Article", "elephant" };
+            // Query the array and select strings according to the selector method.
+            IEnumerable<String> aWords = words.Select(selector);
+
+            // Output the results to the console.
+            foreach (String word in aWords)
+                Console.WriteLine(word);
+
+
+            Get
+            
 
 
             ///SinxtaxeMetodos();
